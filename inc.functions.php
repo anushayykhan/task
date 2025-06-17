01b330482af539f1c4c9d721ec8f19e4194abc06
anushayykhan <anushaykhan389@gmail.com>
2025-06-17 14:38:59 +0500

    Add files via upload

---
+++ inc.functions.php
@@ -0,0 +1,26 @@
+
+<?php
+function pr($data) {
+    echo "<pre>";
+    print_r($data);
+    echo "</pre>";
+}
+
+function prx($data) {
+    echo "<pre>";
+    print_r($data);
+    echo "</pre>";
+    die();
+}
+
+function safe_input($data) {
+    global $conn; // relies on the $conn from inc.connection.php
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    $data = mysqli_real_escape_string($conn, $data);
+    return $data;
+}
+?>
+
+
